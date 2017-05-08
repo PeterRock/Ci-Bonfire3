e762045e601a07f66ee7e47db9ee7f236c57a8ed
Peter Shi <usstszy@163.com>
2017-05-08 10:46:03 +0800

    email template page style

--- bonfire/modules/emailer/views/settings/template.php
+++ bonfire/modules/emailer/views/settings/template.php
@@ -1,30 +1,28 @@
-<p class="well text-primary"><?php echo lang('emailer_template_note'); ?></p>
-<div class="admin-box">
-    <style scoped='scoped'>
-    .admin-box .template {
-        width: 99%;
-    }
-    </style>
+<div class="admin-box container">
+    <p class="well"><?php echo lang('emailer_template_note'); ?></p>
     <?php echo form_open(SITE_AREA . '/settings/emailer/template'); ?>
-        <fieldset>
-            <legend><?php echo lang('emailer_header'); ?></legend>
-			<div class="clearfix">
-				<div class="input">
-					<textarea name="header" rows="15" class="template"><?php echo htmlspecialchars_decode($this->load->view('email/_header', null, true)) ;?></textarea>
-				</div>
-			</div>
-		</fieldset>
-		<fieldset>
-			<legend><?php echo lang('emailer_footer'); ?></legend>
-			<div class="clearfix">
-				<div class="input">
-					<textarea name="footer" rows="15" class="template"><?php echo htmlspecialchars_decode($this->load->view('email/_footer', null, true)) ;?></textarea>
-				</div>
-			</div>
-		</fieldset>
-		<fieldset class="form-group">
-			<input type="submit" name="save" id="submit" class="btn btn-primary" value="<?php e(lang('emailer_save_template')); ?>" />
-            <?php echo ' ' . lang('bf_or') . ' ' . anchor(SITE_AREA . '/settings/emailer', lang('bf_action_cancel')); ?>
-		</fieldset>
-	<?php echo form_close(); ?>
+    <fieldset>
+        <legend><?php echo lang('emailer_header'); ?></legend>
+        <div class="form-group">
+            <div class="input">
+                <textarea name="header" rows="15"
+                          class="form-control"><?php echo htmlspecialchars_decode($this->load->view('email/_header', null, true)); ?></textarea>
+            </div>
+        </div>
+    </fieldset>
+    <fieldset>
+        <legend><?php echo lang('emailer_footer'); ?></legend>
+        <div class="form-group">
+            <div class="input">
+                <textarea name="footer" rows="15"
+                          class="form-control"><?php echo htmlspecialchars_decode($this->load->view('email/_footer', null, true)); ?></textarea>
+            </div>
+        </div>
+    </fieldset>
+    <fieldset class="form-group">
+        <input type="submit" name="save" id="submit" class="btn btn-primary"
+               value="<?php e(lang('emailer_save_template')); ?>"/>
+        <?php echo ' ' . lang('bf_or') . ' ' . anchor(SITE_AREA . '/settings/emailer', lang('bf_action_cancel')); ?>
+    </fieldset>
+    <?php echo form_close(); ?>
 </div>
