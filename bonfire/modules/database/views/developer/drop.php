dc671c9ac227f5f73351c9bdc81b8337278e1f9f
Peter Shi <usstszy@163.com>
2017-05-08 11:37:39 +0800

    update database table browse and drop page style

--- bonfire/modules/database/views/developer/drop.php
+++ bonfire/modules/database/views/developer/drop.php
@@ -1,29 +1,29 @@
-<div class='admin-box drop-table'>
+<div class='admin-box container drop-table'>
     <h3><?php echo lang('database_drop_title'); ?></h3>
-    <?php if (empty($tables) || ! is_array($tables)) : ?>
-    <div class="alert alert-danger">
-        <?php echo lang('database_drop_none'); ?>
-    </div>
-    <?php
+    <?php if (empty($tables) || !is_array($tables)) : ?>
+        <div class="alert alert-danger">
+            <?php echo lang('database_drop_none'); ?>
+        </div>
+        <?php
     else :
         echo form_open(SITE_AREA . '/developer/database/drop');
-    ?>
+        ?>
         <h4><?php echo lang('database_drop_confirm'); ?></h4>
-        <ul>
+        <ol>
             <?php foreach ($tables as $table) : ?>
-            <li><?php e($table); ?>
-                <input type="hidden" name="tables[]" value="<?php e($table); ?>" />
-            </li>
+                <li><?php e($table); ?>
+                    <input type="hidden" name="tables[]" value="<?php e($table); ?>"/>
+                </li>
             <?php endforeach; ?>
-        </ul>
-        <div class="alert alert-warning">
+        </ol>
+        <div class="alert alert-danger">
             <?php echo lang('database_drop_attention'); ?>
         </div>
         <fieldset class="form-group">
             <button type="submit" name="drop" class="btn btn-danger"><?php e(lang('database_drop_button')); ?></button>
-            <?php echo ' ' . lang('bf_or') . ' ' . anchor(SITE_AREA . '/developer/database', lang('bf_action_cancel')); ?>
+            <?php echo ' ' . lang('bf_or') . ' ' . anchor(SITE_AREA . '/developer/database', lang('bf_action_cancel'), 'class="btn btn-default"'); ?>
         </fieldset>
-    <?php
+        <?php
         echo form_close();
     endif;
     ?>
