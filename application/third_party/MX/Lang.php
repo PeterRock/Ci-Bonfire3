80debeb5995b19cea3873f4a3d0c8e07ce867050
PeterRock <usstpeter@gmail.com>
2017-05-14 16:16:42 +0800

    update  codeigniter-modular-extensions-hmvc

--- application/third_party/MX/Lang.php
+++ application/third_party/MX/Lang.php
@@ -14,17 +14,17 @@
  *
  * @copyright	Copyright (c) 2011 Wiredesignz
  * @version 	5.5
- *
+ * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
+ * 
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- *
+ * 
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -42,11 +42,11 @@ class MX_Lang extends CI_Lang
 			foreach($langfile as $_lang) $this->load($_lang);
 			return $this->language;
 		}
-
+			
 		$deft_lang = CI::$APP->config->item('language');
 		$idiom = ($lang == '') ? $deft_lang : $lang;
-
-		if (in_array($langfile.'_lang.php', $this->is_loaded, TRUE))
+	
+		if (in_array($langfile.'_lang'.EXT, $this->is_loaded, TRUE))
 			return $this->language;
 
 		$_module OR $_module = CI::$APP->router->fetch_module();
@@ -55,7 +55,7 @@ class MX_Lang extends CI_Lang
 		if ($path === FALSE) 
 		{
 			if ($lang = parent::load($langfile, $lang, $return, $add_suffix, $alt_path)) return $lang;
-
+		
 		} 
 		else 
 		{
@@ -63,11 +63,11 @@ class MX_Lang extends CI_Lang
 			{
 				if ($return) return $lang;
 				$this->language = array_merge($this->language, $lang);
-				$this->is_loaded[] = $langfile.'_lang.php';
+				$this->is_loaded[] = $langfile.'_lang'.EXT;
 				unset($lang);
 			}
 		}
-
+		
 		return $this->language;
 	}
 }
